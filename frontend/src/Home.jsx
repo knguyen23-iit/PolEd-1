947e3745ded878da0d0dc7ac579e3891eb8c8a63
Ky Nguyen <knguyen23@hawk.iit.edu>
2024-08-27 22:45:36 +0700

    Decorate the Home page

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '@mui/material'
+import './Home.css'
 
 function Home() {
   return (
     <div>
-      <Link to={`/add-filter`}>
-        <button>Step 1: Add Filter</button>
-      </Link>
-      <Link to={`/polaroid`}>
-        <button>Step 2: Polaroid</button>
-      </Link>
+      <div className='button-container'>
+        <Link to={`/add-filter`}>
+          <Button variant='contained'>Step 1: Add Filter</Button>
+        </Link>
+        <Link to={`/polaroid`}>
+          <Button variant='contained'>Step 2: Polaroid</Button>
+        </Link>
+      </div>
+      <div className="logo-container">
+        <img
+          src="blueing.jpg" // Replace with the URL of your image
+          alt="Rotating"
+          className="rotating-image"
+        />
+      </div>
     </div>
   )
 }
